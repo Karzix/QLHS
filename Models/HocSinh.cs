7d04405a1c971ccd6cdd2418834cefcf9ff246b5
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-03-09 12:24:31 +0700

    a

--- Models/HocSinh.cs
+++ Models/HocSinh.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
 
 namespace WebApplication1.Models
 {
     public class HocSinh
     {
         public Guid Id { get; set; }
+        [Display(Name = "Mã số sinh viên")]
         public string MaSV { get; set; }
+        [Display(Name ="Họ và tên")]
         public string HoVaTen { get; set; }
 
         [ForeignKey("LopHocId")]
+        [Display(Name ="Lớp học")]
         public LopHoc? LopHoc { get; set; }
         [ForeignKey("LopHoc")]
         public Guid? LopHocId { get; set; }
--- Models/LopHoc.cs
+++ Models/LopHoc.cs
@@ -1,9 +1,13 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class LopHoc
     {
         public Guid Id { get; set; }
+        [Display(Name = "Mã lớp học")]
         public string MaLopHoc { get; set; }
+        [Display(Name ="Tên lớp học")]
         public string TenLop { get; set; }
     }
 }
