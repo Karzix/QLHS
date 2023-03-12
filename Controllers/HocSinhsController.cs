09d109057d277291997233dc3c87bd92ea08656c
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-03-12 14:45:48 +0700

    thêm icon cho phần sắp xếp ở MaSV

--- Controllers/HocSinhsController.cs
+++ Controllers/HocSinhsController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Models;
+using PagedList;
 
 namespace WebApplication1.Controllers
 {
